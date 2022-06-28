1d9ebef204ac39c76a732714e68a155e30b2b00e
ImBrunoHG <38635661+ImBrunoHG@users.noreply.github.com>
2022-06-28 17:28:20 -0300

    Projeto Cthulhu
    Muito progresso, todas as perícias ja podem fazer o teste agora, mas ainda falta bastante coisa

---
+++ index.js
@@ -0,0 +1,37 @@
+function Roll(t){
+    var rand = Math.floor(Math.random() * 100) + 1
+    document.getElementById('rollimg').style.width = '0%'
+    document.getElementById('rolltxt').hidden = true
+    document.getElementById('result').hidden = true
+    document.getElementById('panel').hidden = false
+    // console.log(rand)
+    setTimeout(() => {
+        document.getElementById('rollimg').style.width = '500px'
+    }, 250);
+    setTimeout(() => {
+        document.getElementById('rolltxt').innerHTML = rand
+        document.getElementById('rolltxt').hidden = false
+        document.getElementById('result').hidden = false
+        document.getElementById('result').innerHTML = checkResult(rand, t.parentNode.getElementsByTagName('input')[0].value)
+    }, 500);
+    // console.log("Normal: " + t.parentNode.getElementsByTagName('input')[0].value + " // Bom: " + Math.floor(t.parentNode.getElementsByTagName('input')[0].value / 2) + " // Extremo: " + Math.floor(t.parentNode.getElementsByTagName('input')[0].value / 5))
+}
+function checkResult(rand, check){
+    var ex = check/5;
+    var go = check/2;
+    if (rand <= ex){
+        return "Extreme"
+    }
+    else if (rand <= go){
+        return "Good"
+    }
+    else if (rand <= check){
+        return "Normal"
+    } else {
+        return "Failure"
+    }
+
+}
+function cvalue(t){
+    t.setAttribute('value', t.value)
+}
