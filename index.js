12dc8999379d9e0abf8356b039ae271b3f851944
ImBrunoHG <38635661+ImBrunoHG@users.noreply.github.com>
2022-07-05 17:30:44 -0300

    Projeto Cthulhu Finalizado (ou quase)
    Falta traduzir pra pt-br

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function Roll(t){
-    var rand = Math.floor(Math.random() * 100) + 1
+    var rand = Ro(100)
     document.getElementById('rollimg').style.width = '0%'
     document.getElementById('rolltxt').hidden = true
     document.getElementById('result').hidden = true
     document.getElementById('panel').hidden = false
-    // console.log(rand)
     setTimeout(() => {
         document.getElementById('rollimg').style.width = '50%'
     }, 250);
@@ -16,14 +15,33 @@ function Roll(t){
     }, 500);
     document.getElementById('using').value = t.parentNode.getElementsByTagName('input')[0].value;
     document.getElementById('using2').innerHTML = "Normal: " + t.parentNode.getElementsByTagName('input')[0].value + "<br>Good: " + Math.floor(t.parentNode.getElementsByTagName('input')[0].value/2) + "<br>Extreme: " + Math.floor(t.parentNode.getElementsByTagName('input')[0].value/5);
-    // console.log("Normal: " + t.parentNode.getElementsByTagName('input')[0].value + " // Bom: " + Math.floor(t.parentNode.getElementsByTagName('input')[0].value / 2) + " // Extremo: " + Math.floor(t.parentNode.getElementsByTagName('input')[0].value / 5))
+    document.getElementById('rerollbtn').setAttribute('onclick', 'Roll(this)')
+}
+function RollCustom(){
+    var rand = R(document.getElementById('customd').value)
+    document.getElementById('rollimg').style.width = '0%'
+    document.getElementById('rolltxt').hidden = true
+    document.getElementById('result').hidden = true
+    document.getElementById('panel').hidden = false
+    setTimeout(() => {
+        document.getElementById('rollimg').style.width = '50%'
+    }, 250);
+    setTimeout(() => {
+        document.getElementById('rolltxt').innerHTML = rand[0]
+        document.getElementById('rolltxt').hidden = false
+        document.getElementById('using2').innerHTML = "Rolagem: " + document.getElementById('customd').value + "<br>" + rand[1];
+    }, 500);
+    document.getElementById('using').value = document.getElementById('customd').value;
+    document.getElementById('using2').innerHTML = "Rolagem: " + document.getElementById('customd').value;
+    document.getElementById('rerollbtn').setAttribute('onclick', 'RollCustom()')
 }
 function R(s){
-    console.log(s.split("d"))
     var s2 = s.split("d")
+    s2 = s2.flat(5)
+    var res = 0;
+    var showRes = '';
     for(var i = 0; i< s2.length; i++){
         var s3;
-        //Surtei, mas tem que verificar se é array pra resolver o problema que tá dando quando coloca algo tipo "2d4+3-2d4"
         if (s2[i].search(/\+/) != -1){
             s3 = s2[i].split('+')
             for(var e = 1; e < s3.length; e+=2){
@@ -31,32 +49,94 @@ function R(s){
             }
             s2[i] = s3
         }
-        else if (s2[i].search(/\-/) != -1){
-            if(Array.isArray(s2[i])){
-                for(var o = 0; o < s2[i].length; o++){
-                    var s4;
-                    s4 = s2[i].split('-')
-                    for(var e = 1; e < s4.length; e+=2){
-                        s4.splice(e, 0, "-")
+    }
+    s2 = s2.flat(5)
+    for(var i = 0; i < s2.length; i++){
+        if (s2[i].search(/\-/) != -1){
+            s3 = s2[i].split('-')
+            for(var e = 1; e < s3.length; e+=2){
+                s3.splice(e, 0, "-")
+            }
+            s2[i] = s3
+        }
+    }
+    s2 = s2.flat(5)
+    for(var i = 0; i < s2.length; i++){
+        if (s2[i].search(/\//) != -1){
+            s3 = s2[i].split('/')
+            for(var e = 1; e < s3.length; e+=2){
+                s3.splice(e, 0, "/")
+            }
+            s2[i] = s3
+        }
+    }
+    s2 = s2.flat(5)
+    for(var i = 0; i < s2.length; i++){
+        if (s2[i].search(/\*/) != -1){
+            s3 = s2[i].split('*')
+            for(var e = 1; e < s3.length; e+=2){
+                s3.splice(e, 0, "*")
+            }
+            s2[i] = s3
+        }
+    }
+    s2 = s2.flat(5)
+    for(var i = 0; i<s2.length; i++){
+        if(s2[i] == ''){
+            s2[i] = '1'
+        }
+    }
+    for (var i = 0; i< s2.length; i++){
+        if (s2[i] == '+' || s2[i] == '-' || s2[i] == '/' || s2[i] == '*'){
+            if (s2[i+2] == '+' || s2[i+2] == '-' || s2[i+2] == '/' || (i+2) >= s2.length || s2[i+2] == "*"){
+                if(s2[i] == '+'){
+                    res += parseFloat(s2[i+1])
+                    showRes += ' + [' + s2[i+1] + ']' 
+                } else if (s2[i] == '-'){
+                    res -= parseFloat(s2[i+1])
+                    showRes += ' - [' + s2[i+1] + ']' 
+                } else if (s2[i] == '/'){
+                    res = Math.floor(res / parseFloat(s2[i+1]))
+                    showRes += ' / [' + s2[i+1] + ']' 
+                } else if (s2[i] == '*'){
+                    res = Math.floor(res * parseFloat(s2[i+1]))
+                    showRes += ' * [' + s2[i+1] + ']' 
+                }
+            }
+        }
+        else {
+            if ((i+1) < s2.length){
+                if(s2[i+1] == '+' || s2[i+1] == '-' || s2[i+1] == '/' || s2[i+1] == '*'){
+
+                }
+                else {
+                    var rollRes = 0;
+                    var rollDices = [];
+                    if (s2[i-1] == '-'){
+                        showRes += ' - '
+                    } else if (s2[i-1] == '+'){
+                        showRes += ' + '
                     }
-                    s3[o] = s4
+                    showRes += '['
+                    for(var o = 0; o<s2[i]; o++){
+                        var r = Ro(s2[i+1])
+                        if (s2[i-1] == '-'){
+                            rollRes -= r
+                            showRes += r + ', ';
+                        }else{
+                            rollRes += r
+                            showRes += r + ', ';
+                        }
+                        rollDices[rollDices.length] = r
+                    }
+                    showRes = showRes.slice(0, -2)
+                    showRes += ']'
+                    res += rollRes;
                 }
-                s2[i] = s3
-            } else {
-                s3 = s2[i].split('-')
-                for(var e = 1; e < s3.length; e+=2){
-                    s3.splice(e, 0, "-")
-                }
-                s2[i] = s3
             }
         }
     }
-    console.log(s2)
-        // if(Array.isArray(s2[i])){
-        //     for (var e = 0; e < s2[i-1]; e++){
-        //         Ro(s2)
-        //     }
-        // }
+    return [res, showRes];
 }
 function Ro(q){
     return Math.floor(Math.random() * q) + 1
@@ -86,4 +166,7 @@ function checkResult(rand, check){
 }
 function cvalue(t){
     t.setAttribute('value', t.value)
+}
+function save(t){
+    console.log(document)
 }
