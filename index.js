1284db5187665767563bf2e27fb42dfe8e763782
ImBrunoHG <38635661+ImBrunoHG@users.noreply.github.com>
2022-07-13 17:21:08 -0300

    Projeto Cthulhu (Update 13.07)
    Adicionado linguagens para as perícias

--- index.js
+++ index.js
@@ -178,32 +178,38 @@ function save() {
                 { type: "text/plain;charset=utf-8" });
     saveAs(blob, n + ".sheet")
 }
-createSkills();
-function createSkills(){
+function createSkills(l, t){
+    document.getElementById('skills').innerHTML = '<h2>Perícias</h2>\n<div style="clear: both;"></div>\n<div style="clear: both;" class="centralized container" id="customDDiv">\n<input style="display: inline-block;" class="form-control centralized diceC" type="text" name="custom" id="customd" placeholder="Dados (Ex: 1d4+1d8+5)">\n<button class="btn btn-primary" onclick="RollCustom()">Roll</button>\n</div>'
+    var sk;
+    var in1 = document.createElement('div')
+    in1.className = 'innerWin container sk'
+    document.getElementById('skills').insertBefore(in1, document.getElementById('customDDiv'))
+    var in2 = document.createElement('div')
+    in2.className = 'innerWin container sk'
+    document.getElementById('skills').insertBefore(in2, document.getElementById('customDDiv'))
+    var in3 = document.createElement('div')
+    in3.className = 'innerWin container sk'
+    document.getElementById('skills').insertBefore(in3, document.getElementById('customDDiv'))
+    var in4 = document.createElement('div')
+    in4.className = 'innerWin container sk'
+    document.getElementById('skills').insertBefore(in4, document.getElementById('customDDiv'))
     var skpt = [
         ["Antropologia", 1, false], 
         ["Armas de Fogo (Pistolas)", 20, false], 
         ["Armas de Fogo (Rifles)", 25, false],
+        ["Armas 1", 25, true],
         ["Arqueologia", 1, false],
         ["Arremessar", 20, false],
-        ["Armas 1", 25, true],
         ["Arte/Ofício 1", 5, true],
         ["Arte/Ofício 2", 5, true],
         ["Arte/Ofício 3", 5, true],
-        ["Ciência 1", 1, true],
-        ["Ciência 2", 1, true],
-        ["Idioma (Nativo)", 0, true],
-        ["Idioma 1", 1, true],
-        ["Idioma 2", 1, true],
-        ["Idioma 3", 1, true],
-        ["Luta 1", 25, true],
-        ["Luta 2", 25, true],
-        ["Sobrevivência", 10, true],
         ["Avaliação", 5, false],
         ["Cavalgar", 5, false],
         ["Charme", 15, false],
         ["Chaveiro", 1, false],
         ["Ciência", 1, false],
+        ["Ciência 1", 1, true],
+        ["Ciência 2", 1, true],
         ["Consertos Elétricos", 10, false],
         ["Consertos Mecânicos", 5, false],
         ["Contabilidade", 5, false],
@@ -211,33 +217,164 @@ function createSkills(){
         ["Dirigir Automóvel", 20, false],
         ["Disfarce", 5, false],
         ["Encontrar", 25, false],
-        ["Escutar", 20, false],
+        ["Idioma (Nativo)", 0, true],
         ["Escalar", 20, false],
+        ["Escutar", 20, false],
         ["Esquivar", 0, false],
-        ["Lábia", 5, false],
-        ["Intimidação", 15, false],
-        ["História", 5, false],
         ["Furtividade", 20, false],
+        ["História", 5, false],
+        ["Idioma 1", 1, true],
+        ["Idioma 2", 1, true],
+        ["Idioma 3", 1, true],
+        ["Intimidação", 15, false],
+        ["Lábia", 5, false],
         ["Luta (Briga)", 25, false],
+        ["Luta 1", 25, true],
+        ["Luta 2", 25, true],
         ["Medicina", 1, false],
-        ["Mythos de Cthulhu", 0, false],
         ["Mundo Natural", 10, false],
+        ["Mythos de Cthulhu", 0, false],
         ["Natação", 20, false],
         ["Navegação", 10, false],
         ["Nível de Crédito", 0, false],
         ["Ocultismo", 5, false],
         ["Operar Maquinário Pesado", 1, false],
         ["Persuasão", 10, false],
-        ["Pilotar", 1, false],
+        ["Pilotar", 1, true],
         ["Prestidigitação", 10, false],
         ["Primeiros Socorros", 30, false],
         ["Psicanálise", 1, false],
         ["Psicologia", 10, false],
-        ["Saltar", 20, false],
         ["Rastrear", 10, false],
-        ["Usar Bibliotecas", 20, false]
+        ["Saltar", 20, false],
+        ["Sobrevivência", 10, true],
+        ["Usar Bibliotecas", 20, false],
+        ["Custom", 0, true],
+        ["Custom", 0, true],
+        ["Custom", 0, true],
+        ["Custom", 0, true],
+        ["Custom", 0, true]
     ];
-    skpt.sort();
-    console.log(skpt)
-    var sken = [];
+    var sken = [
+        ["Accounting", 5, false],
+        ["Anthropology", 1, false], 
+        ["Appraise", 5, false],
+        ["Archaeology", 1, false],
+        ["Art/Craft 1", 5, true],
+        ["Art/Craft 2", 5, true],
+        ["Art/Craft 3", 5, true],
+        ["Charm", 15, false],
+        ["Climb", 20, false],
+        ["Credit Rating", 0, false],
+        ["Cthulhu Mythos", 0, false],
+        ["Disguise", 5, false],
+        ["Dodge", 0, false],
+        ["Drive Automobile", 20, false],
+        ["Electric Repair", 10, false],
+        ["Fast Talk", 5, false],
+        ["Fighting (Brawl)", 25, false],
+        ["Fight 1", 25, true],
+        ["Fight 2", 25, true],
+        ["Firearms (Handguns)", 20, false], 
+        ["Firearms (Rifles)", 25, false],
+        ["Firearms 1", 25, true],
+        ["First Aid", 30, false],
+        ["History", 5, false],
+        ["Intimidate", 15, false],
+        ["Jump", 20, false],
+        ["(Own) Language", 0, true],
+        ["Language 1", 1, true],
+        ["Language 2", 1, true],
+        ["Language 3", 1, true],
+        ["Law", 5, false],
+        ["Library Use", 20, false],
+        ["Listen", 20, false],
+        ["Locksmith", 1, false],
+        ["Mechanical Repair", 5, false],
+        ["Medicine", 1, false],
+        ["Natural World", 10, false],
+        ["Navigate", 10, false],
+        ["Occult", 5, false],
+        ["Operate Heavy Machine", 1, false],
+        ["Persuade", 10, false],
+        ["Pilot", 1, true],
+        ["Psychanalysis", 1, false],
+        ["Psychology", 10, false],
+        ["Ride", 5, false],
+        ["Science", 1, false],
+        ["Science 1", 1, true],
+        ["Science 2", 1, true],
+        ["Sleight of Hand", 10, false],
+        ["Spot Hidden", 25, false],
+        ["Stealth", 20, false],
+        ["Survival", 10, true],
+        ["Swim", 20, false],
+        ["Throw", 20, false],
+        ["Track", 10, false],
+        ["Custom", 0, true],
+        ["Custom", 0, true],
+        ["Custom", 0, true],
+        ["Custom", 0, true],
+        ["Custom", 0, true]
+    ];
+    if (l == 'pt-br'){
+        sk = skpt;
+    } else if (l == 'en-us'){
+        sk = sken;
+    }
+    for (var i = 0; i < sk.length; i++){
+        var div1 = document.createElement('div')
+        div1.setAttribute('name', "sk" + i)
+        div1.style = "margin-bottom: 10px; padding: 10px; overflow-x: auto;"
+        div1.className = 'container ininnerWin'
+        var div2 = document.createElement('div')
+        div2.style = 'clear: both;'
+        var div3 = document.createElement('div')
+        div3.style = "width: 45%;"
+        if (sk[i][2]){
+            var inp = document.createElement('input')
+            inp.setAttribute('onchange', 'cvalue(this)')
+            inp.className = 'form-control inp'
+            inp.style = 'float: left; margin-right: 10px;'
+            inp.placeholder = '(' + sk[i][0] + ')'
+            div3.appendChild(inp)
+        } else {
+            var h5 = document.createElement('h5')
+            h5.style = 'float: left; margin-right: 10px;'
+            h5.innerHTML = sk[i][0]
+            div3.appendChild(h5)
+        }
+        var div4 = document.createElement('div')
+        var btn = document.createElement('button')
+        btn.style = 'float: right; margin-left: 5px;'
+        btn.className = 'btn btn-primary'
+        btn.setAttribute('onclick', 'Roll(this)')
+        btn.innerHTML = 'Roll'
+        var inp2 = document.createElement('input')
+        inp2.setAttribute('onchange', 'cvalue(this)')
+        inp2.type = 'number'
+        inp2.name = 'sk' + i
+        inp2.id = 'sk' + i
+        inp2.className = 'form-control inp'
+        inp2.style = 'float: right; width: 10%;'
+        inp2.value = sk[i][1]
+        var div5 = document.createElement('div')
+        div5.style = 'clear: both;'
+        div4.appendChild(btn)
+        div4.appendChild(inp2)
+        div1.appendChild(div2)
+        div1.appendChild(div3)
+        div1.appendChild(div4)
+        div1.appendChild(div5)
+        if (i < 15){
+            in1.appendChild(div1)
+        } else if (i < 30){
+            in2.appendChild(div1)
+        } else if (i < 45){
+            in3.appendChild(div1)
+        } else if (i < 60){
+            in4.appendChild(div1)
+        }
+    }
+    console.log(sk)
 }
